9e679c71b1b73ca1838c3d33bb46ab55467c4cb4
Oleksiy Nosov <oleksiynosov@gmail.com>
2021-10-31 21:56:57 +0200

    Improve bird control

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.EnhancedTouch;
-using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch; 
 
 public class Bird : MonoBehaviour
 {
     [SerializeField] float jumpForce = 5f;
 
     private Rigidbody2D rigidBody;
+
+    private bool isReadyForJump = true;
 
     private void Start()
     {
@@ -18,14 +18,24 @@ public class Bird : MonoBehaviour
 
     private void Update()
     {
-        var touchCount = Touch.activeTouches.Count;
-        var isTouching = touchCount > 0;
+        var isTouching = Touchscreen.current.primaryTouch.press.isPressed;
 
-        if (isTouching)
+        if (!isTouching && !isReadyForJump)
         {
+            isReadyForJump = true;
+
+            return;
+        }
+
+        if (isTouching && isReadyForJump)
+        {
+            isReadyForJump = false;
+
             var jumpVector = Vector2.up * jumpForce;
-            
+
             rigidBody.velocity += jumpVector;
+
+            return;
         }
     }
 }
